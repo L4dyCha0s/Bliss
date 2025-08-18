30f9edc1964490f404576a3f5725a01361553a1d
L4dyCha0s <devilsatella@gmail.com>
2025-08-18 20:02:31 -0300

    SAIDERS 1

---
+++ commands/isa.js
@@ -0,0 +1,3 @@
+module.exports = async (client, msg) => {
+  await msg.reply('A maior Lesbica que temos. Isa, é um prazer! Me ensina a conquistar as garotas como você?'); 
+};
---
+++ commands/maletta.js
@@ -0,0 +1,3 @@
+module.exports = async (client, msg) => {
+  await msg.reply('Ah, o nobre e reservado maletta, saudações. QUal a próxima pérola que você dirá para os novatos? Estamos curiosos a respeito.'); 
+};

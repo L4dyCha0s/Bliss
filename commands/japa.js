06e900a1a51809c9c830c5d0f7d544815ebd2ae1
L4dyCha0s <devilsatella@gmail.com>
2025-08-19 18:18:47 -0300

    .

---
+++ commands/japa.js
@@ -0,0 +1,3 @@
+module.exports = async (client, msg) => {
+  await msg.reply('Japa, o injustiçado, de friendzone em friendzone... Mas fique tranquilo, uma hora o amor há de chegar. Tente digitar "!sorteio" para sortear um amor para hoje. '); 
+};
---
+++ commands/sasha.js
@@ -0,0 +1,3 @@
+module.exports = async (client, msg) => {
+  await msg.reply('Conforme dados do IBGE 2025, Sasha Botelho Bolha já se casou com metade do saidinha. \n \n Dados registrados até a primeira metade do ano. Quem sabe até o final do ano ela não casa com o resto...'); 
+};
